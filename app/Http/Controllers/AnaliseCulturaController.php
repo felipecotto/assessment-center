38c886235c095fef6a132e98314ffb3a4e02627d
Anderson Fujihara <fujihara.anderson@gmail.com>
2017-10-17 15:19:02 -0200

    Update AnaliseCulturaController.php

--- app/Http/Controllers/AnaliseCulturaController.php
+++ app/Http/Controllers/AnaliseCulturaController.php
@@ -89,7 +89,43 @@ class AnaliseCulturaController extends Controller
         $response->{'6_D_1'} = Input::get('6_D_1');
         $response->{'6_D_2'} = Input::get('6_D_2');
         $response->save();
-        return redirect('finalizada');
+        
+        $atual_a = $response->{'1_A_1'} + $response->{'2_A_1'} + $response->{'3_A_1'} + $response->{'4_A_1'} +
+        $response->{'5_A_1'} + $response->{'6_A_1'};  
+
+        $atual_b = $response->{'1_B_1'} + $response->{'2_B_1'} + $response->{'3_B_1'} + $response->{'4_B_1'} +
+        $response->{'5_B_1'} + $response->{'6_B_1'};
+
+        $atual_c = $response->{'1_C_1'} + $response->{'2_C_1'} + $response->{'3_C_1'} + $response->{'4_C_1'} +
+        $response->{'5_C_1'} + $response->{'6_C_1'};
+
+        $atual_d = $response->{'1_D_1'} + $response->{'2_D_1'} + $response->{'3_D_1'} + $response->{'4_D_1'} +
+        $response->{'5_D_1'} + $response->{'6_D_1'};
+
+        $preferida_a = $response->{'1_A_2'} + $response->{'2_A_2'} + $response->{'3_A_2'} + $response->{'4_A_2'} +
+        $response->{'5_A_2'} + $response->{'6_A_2'};  
+
+        $preferida_b = $response->{'1_B_2'} + $response->{'2_B_2'} + $response->{'3_B_2'} + $response->{'4_B_2'} +
+        $response->{'5_B_2'} + $response->{'6_B_2'};
+
+        $preferida_c = $response->{'1_C_2'} + $response->{'2_C_2'} + $response->{'3_C_2'} + $response->{'4_C_2'} +
+        $response->{'5_C_2'} + $response->{'6_C_2'};
+
+        $preferida_d = $response->{'1_D_2'} + $response->{'2_D_2'} + $response->{'3_D_2'} + $response->{'4_D_2'} +
+        $response->{'5_D_2'} + $response->{'6_D_2'};
+        
+        return view('analise-cultura.results_final')
+            ->with([
+                'response'=>$response, 
+                'atual_a'=>$atual_a, 
+                'atual_b'=>$atual_b, 
+                'atual_c'=>$atual_c, 
+                'atual_d'=>$atual_d,
+                'preferida_a'=>$preferida_a, 
+                'preferida_b'=>$preferida_b, 
+                'preferida_c'=>$preferida_c, 
+                'preferida_d'=>$preferida_d
+            ]); 
     }
 
     public function edit() {
